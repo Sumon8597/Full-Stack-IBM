bdbd10bf52f4207460f274375fcc509bf58ba7ad
Sumon8597 <pal395414@gmail.com>
2025-02-24 13:29:21 +0530

    day9assignment

---
+++ Day 9/Classwork/Day9.js
@@ -0,0 +1,59 @@
+// {let a=10;
+//     console.log(a);
+// }   
+
+// let a=10,b="10"
+// console.log(typeof(a))
+// console.log(typeof(b))
+// b=+b
+// if(a===b){
+//     console.log("a is equal to b")
+// }else{
+//     console.log("a is not equal to b")
+// }
+
+// function myfunction(a,b){
+//     console.log(a+b)
+// }
+// let a=10;
+// let b=20;
+// myfunction(a,b)
+
+//myFunction(a,b)
+
+
+// let a=10;
+// let b=20;
+
+// const myFunction=(a,b)=>{
+//     console.log(a+b)
+// }
+// myFunction(a,b)
+
+// console.log(name)
+// var name="alexendar"
+
+// let $name ="sumon";
+// let _name2="pal";
+// console.log($name)
+
+
+myName()
+
+console.log(a)
+var a=10;
+
+function myName(){
+  
+    console.log(a)
+}
+
+// let d="      "
+// console.log(d.length)
+
+let fname="sumon"
+let Iname="pal"
+console.log(fname,Iname)
+let b=20 , c=+"30"
+
+console.log(b+c)
