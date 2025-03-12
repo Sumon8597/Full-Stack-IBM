13889fc8745b71eddf5d0fbdf8560d35caecd6fd
Sumon8597 <pal395414@gmail.com>
2025-03-12 20:53:24 +0530

    day20 work

---
+++ Day20/Classwork/index.js
@@ -0,0 +1,8 @@
+const os =require("os");
+
+const http = require("http");
+console.log(http);
+// console.log(os.homedir());
+// console.log(os.machine());
+// console.log(os.cpus());
+// console.log(os.freemem());
